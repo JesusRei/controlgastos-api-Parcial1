082e2afe5028b01f76992c311cbfe92f6f125410
Jesus David Reinoso Gualtero <jesus.reinoso27587@ucaldas.edu.co>
2024-10-21 23:50:30 -0500

    controlgastos-api-Parcial1

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GastoController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\CategoriaController;
 
 // Rutas para el controlador GastoController
 Route::apiResource('gastos', GastoController::class)
+    ->middleware('auth:sanctum'); // Protege las rutas con autenticación
+
+// Rutas para el controlador CategoriaController
+Route::apiResource('categorias', CategoriaController::class)
     ->middleware('auth:sanctum'); // Protege las rutas con autenticación
 
 // Rutas para el controlador LoginController
